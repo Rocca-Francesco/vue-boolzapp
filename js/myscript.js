b21e7292c67a0902fee443f72caad0aecf7fc33d
Rocca-Francesco <roccafrance98@yahoo.it>
2023-02-16 18:05:29 +0100

    label delete message in progress

--- js/myscript.js
+++ js/myscript.js
@@ -12,17 +12,23 @@ createApp({
             {
               date: '10/01/2020 15:30:55',
               text: 'Hai portato a spasso il cane?',
-              status: 'sent'
+              status: 'sent',
+              // variabile per vedere se l'utente sta cercando di selezionare un messaggio
+              upHere: false
             },
             {
               date: '10/01/2020 15:50:00',
               text: 'Ricordati di stendere i panni',
-              status: 'sent'
+              status: 'sent',
+              // variabile per vedere se l'utente sta cercando di selezionare un messaggio
+              upHere: false
             },
             {
               date: '10/01/2020 16:15:22',
               text: 'Tutto fatto!',
-              status: 'received'
+              status: 'received',
+              // variabile per vedere se l'utente sta cercando di selezionare un messaggio
+              upHere: false
             }
           ],
         },
@@ -34,17 +40,23 @@ createApp({
             {
               date: '20/03/2020 16:30:00',
               text: 'Ciao come stai?',
-              status: 'sent'
+              status: 'sent',
+              // variabile per vedere se l'utente sta cercando di selezionare un messaggio
+              upHere: false
             },
             {
               date: '20/03/2020 16:30:55',
               text: 'Bene grazie! Stasera ci vediamo?',
-              status: 'received'
+              status: 'received',
+              // variabile per vedere se l'utente sta cercando di selezionare un messaggio
+              upHere: false
             },
             {
               date: '20/03/2020 16:35:00',
               text: 'Mi piacerebbe ma devo andare a fare la spesa.',
-              status: 'sent'
+              status: 'sent',
+              // variabile per vedere se l'utente sta cercando di selezionare un messaggio
+              upHere: false
             }
           ],
         },
@@ -56,17 +68,23 @@ createApp({
             {
               date: '28/03/2020 10:10:40',
               text: 'La Marianna va in campagna',
-              status: 'received'
+              status: 'received',
+              // variabile per vedere se l'utente sta cercando di selezionare un messaggio
+              upHere: false
             },
             {
               date: '28/03/2020 10:20:10',
               text: 'Sicuro di non aver sbagliato chat?',
-              status: 'sent'
+              status: 'sent',
+              // variabile per vedere se l'utente sta cercando di selezionare un messaggio
+              upHere: false
             },
             {
               date: '28/03/2020 16:15:22',
               text: 'Ah scusa!',
-              status: 'received'
+              status: 'received',
+              // variabile per vedere se l'utente sta cercando di selezionare un messaggio
+              upHere: false
             }
           ],
         },
@@ -78,12 +96,16 @@ createApp({
             {
               date: '10/01/2020 15:30:55',
               text: 'Lo sai che ha aperto una nuova pizzeria?',
-              status: 'sent'
+              status: 'sent',
+              // variabile per vedere se l'utente sta cercando di selezionare un messaggio
+              upHere: false
             },
             {
               date: '10/01/2020 15:50:00',
               text: 'Si, ma preferirei andare al cinema',
-              status: 'received'
+              status: 'received',
+              // variabile per vedere se l'utente sta cercando di selezionare un messaggio
+              upHere: false
             }
           ],
         },
@@ -95,12 +117,16 @@ createApp({
             {
               date: '10/01/2020 15:30:55',
               text: 'Ricordati di chiamare la nonna',
-              status: 'sent'
+              status: 'sent',
+              // variabile per vedere se l'utente sta cercando di selezionare un messaggio
+              upHere: false
             },
             {
               date: '10/01/2020 15:50:00',
               text: 'Va bene, stasera la sento',
-              status: 'received'
+              status: 'received',
+              // variabile per vedere se l'utente sta cercando di selezionare un messaggio
+              upHere: false
             }
           ],
         },
@@ -112,17 +138,23 @@ createApp({
             {
               date: '10/01/2020 15:30:55',
               text: 'Ciao Claudia, hai novità?',
-              status: 'sent'
+              status: 'sent',
+              // variabile per vedere se l'utente sta cercando di selezionare un messaggio
+              upHere: false
             },
             {
               date: '10/01/2020 15:50:00',
               text: 'Non ancora',
-              status: 'received'
+              status: 'received',
+              // variabile per vedere se l'utente sta cercando di selezionare un messaggio
+              upHere: false
             },
             {
               date: '10/01/2020 15:51:00',
               text: 'Nessuna nuova, buona nuova',
-              status: 'sent'
+              status: 'sent',
+              // variabile per vedere se l'utente sta cercando di selezionare un messaggio
+              upHere: false
             }
           ],
         },
@@ -134,12 +166,16 @@ createApp({
             {
               date: '10/01/2020 15:30:55',
               text: 'Fai gli auguri a Martina che è il suo compleanno!',
-              status: 'sent'
+              status: 'sent',
+              // variabile per vedere se l'utente sta cercando di selezionare un messaggio
+              upHere: false
             },
             {
               date: '10/01/2020 15:50:00',
               text: 'Grazie per avermelo ricordato, le scrivo subito!',
-              status: 'received'
+              status: 'received',
+              // variabile per vedere se l'utente sta cercando di selezionare un messaggio
+              upHere: false
             }
           ],
         },
@@ -151,17 +187,23 @@ createApp({
             {
               date: '10/01/2020 15:30:55',
               text: 'Ciao, andiamo a mangiare la pizza stasera?',
-              status: 'received'
+              status: 'received',
+              // variabile per vedere se l'utente sta cercando di selezionare un messaggio
+              upHere: false
             },
             {
               date: '10/01/2020 15:50:00',
               text: 'No, l\'ho già mangiata ieri, ordiniamo sushi!',
-              status: 'sent'
+              status: 'sent',
+              // variabile per vedere se l'utente sta cercando di selezionare un messaggio
+              upHere: false
             },
             {
               date: '10/01/2020 15:51:00',
               text: 'OK!!',
-              status: 'received'
+              status: 'received',
+              // variabile per vedere se l'utente sta cercando di selezionare un messaggio
+              upHere: false
             }
           ],
         }
@@ -174,6 +216,7 @@ createApp({
       userAnswer: false,
       // variabile per intercettare la ricerca
       currentSearch: "",
+
     }
 
   },
@@ -192,7 +235,9 @@ createApp({
       this.contacts[this.activeList].messages.push({
         date: '10/01/2020 15:51:00',
         text: newMessage,
-        status: 'sent'
+        status: 'sent',
+        // variabile per vedere se l'utente sta cercando di selezionare un messaggio
+        upHere: false
       });
       // attivo la risposta
       this.userAnswer = true;
@@ -207,7 +252,9 @@ createApp({
           this.contacts[this.activeList].messages.push({
             date: '10/01/2020 15:51:00',
             text: 'Ok',
-            status: 'received'
+            status: 'received',
+            // variabile per vedere se l'utente sta cercando di selezionare un messaggio
+            upHere: false
           });
           this.userAnswer = false;
         }, 1000);
@@ -249,5 +296,14 @@ createApp({
       };
 
     },
+
+    showMe(index) {
+      if (index)
+        this.upHere = true;
+    },
+
+    dontShowMe(index) {
+      this.upHere = false;
+    }
   }
 }).mount('#app')
